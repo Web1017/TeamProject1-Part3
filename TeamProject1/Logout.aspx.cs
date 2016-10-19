3041117198fd69deb3dec0e7d6771f1fbd574dd1
Web1017 <web1017@hotmail.com>
2016-10-19 13:06:25 -0400

    Added logout/Register

--- TeamProject1/Logout.aspx.cs
+++ TeamProject1/Logout.aspx.cs
@@ -5,12 +5,27 @@ using System.Web;
 using System.Web.UI;
 using System.Web.UI.WebControls;
 
+//required for Identity and OWIN Security
+using Microsoft.AspNet.Identity;
+using Microsoft.AspNet.Identity.EntityFramework;
+using Microsoft.Owin.Security;
+
 namespace TeamProject1
 {
     public partial class Logout : System.Web.UI.Page
     {
         protected void Page_Load(object sender, EventArgs e)
         {
+            //store sessions info and authentication methods in the authenticationmanager object
+
+            var authenticationManager = HttpContext.Current.GetOwinContext().Authentication;
+
+            //perform sign out
+            authenticationManager.SignOut();
+
+            //redirect user to Login Page
+
+            Response.Redirect("~/Login.aspx");
 
         }
     }
--- TeamProject1/Register.aspx.cs
+++ TeamProject1/Register.aspx.cs
@@ -5,6 +5,11 @@ using System.Web;
 using System.Web.UI;
 using System.Web.UI.WebControls;
 
+//Required for Identity and OWIN
+using Microsoft.AspNet.Identity;
+using Microsoft.AspNet.Identity.EntityFramework;
+using Microsoft.Owin.Security;
+
 namespace TeamProject1
 {
     public partial class Register : System.Web.UI.Page
@@ -16,11 +21,51 @@ namespace TeamProject1
 
         protected void CancelButton_Click(object sender, EventArgs e)
         {
+            //Redirect back to the Default Page
+            Response.Redirect("~/Default.aspx");
+
 
         }
 
         protected void RegisterButton_Click(object sender, EventArgs e)
         {
+            //create a new userStore and UserManager Objects
+            var userStore = new UserStore<IdentityUser>();
+            var userManager = new UserManager<IdentityUser>(userStore);
+
+            //create a new user object
+            var user = new IdentityUser()
+            {
+                
+                UserName = UserNameTextBox.Text,
+                PhoneNumber = PhoneNumberTextBox.Text,
+                Email = EmailTextBox.Text
+            };
+
+            // create a new user in the database and store the results
+            IdentityResult result = userManager.Create(user, PasswordTextBox.Text);
+
+            //Check if successfullyregistered?
+
+            if(result.Succeeded)
+            {
+                //authenticate and login our new user
+                var authenticationManager = HttpContext.Current.GetOwinContext().Authentication;
+                var userIdentity = userManager.CreateIdentity(user, DefaultAuthenticationTypes.ApplicationCookie);
+
+                //sign in the user
+                authenticationManager.SignIn(new AuthenticationProperties() { }, userIdentity);
+
+                //Redirect to the Main Menu Page
+                Response.Redirect("~/GameTrack/MainMenu.aspx");
+            }
+            else
+            {
+                //Display error in the AlerFlash Div
+                StatusLabel.Text = result.Errors.FirstOrDefault();
+                AlertFlash.Visible = true;
+            }
+
 
         }
     }
